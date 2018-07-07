d2c061e29d88f8a2a2ceea0ce247b225897c89eb
jasonportilla <jasonportilla@Jasons-MacBook-Pro.local>
2018-07-07 13:03:15 -0400

    added mariaDB to project with connection

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import './assets/index.scss'
+import './assets/css/index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
