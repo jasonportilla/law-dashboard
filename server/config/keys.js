9bd4547de8563e7ff8fe1fc0888632def2eb7414
jasonportilla <jasonportilla@Jasons-MacBook-Pro.local>
2018-07-19 23:00:40 -0400

    added config files for database info

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	awsURI: 'law-dash.c9os02zlzyuu.us-east-1.rds.amazonaws.com',
+	user: 'portillaj',
+	password: 'port6911',
+	database: 'law_dashboard',
+};
--- server/db.js
+++ server/db.js
@@ -1,27 +1,26 @@
 const mysql = require('mysql');
+const keys = require('./config/keys');
 
-let state = {
-    pool: null
+const state = {
+	pool: null,
 };
 
 
 //This will be the public function to create a mysql connection
-exports.connect = function(mode, done){
+exports.connect = function (mode) {
 
-        //This is the current state variable which will be the 
-        //mysql pool 
-        state.pool = mysql.createPool({
-        host:       'law-dash.c9os02zlzyuu.us-east-1.rds.amazonaws.com',
-        user:       'portillaj',
-        password:   'port6911',
-        database:   'law_dashboard',
-        port: 3306
-    });
-
+	//This is the current state variable which will be the
+	//mysql pool
+	state.pool = mysql.createPool({
+		host: keys.awsURI,
+		user: keys.user,
+		password: keys.password,
+		database: keys.database,
+	});
 };
 
-//This get function will be called by the data model to 
+//This get function will be called by the data model to
 //get the state pool connect and to execute the query
-exports.get = function(){
-    return state.pool;
-}
+exports.get = function () {
+	return state.pool;
+};
