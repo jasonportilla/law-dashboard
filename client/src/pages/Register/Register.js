04b3b4fad6acee82ba88121eacf54cfdaf8dc782
jasonportilla <jasonportilla@portillaj.local>
2018-08-24 22:00:18 -0400

    added Register page skeleton to work on getting a user registered

---
+++ client/src/pages/Register/Register.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Register Page
+        </h1>
+      </div>
+    )
+  }
+};
+
+Register.propTypes = {
+  
+}
+
+export default Register;
