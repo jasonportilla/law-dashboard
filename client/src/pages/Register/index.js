6c4e48681ed0e7781b0a736ecdae40e1e661a242
jasonportilla <jasonportilla@portillaj.local>
2018-08-31 19:28:41 -0400

    added registration page with input.  need to figure out grid system to add the picture

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
-
+import Button from '../../components/materialComponents/CustomButtons/Button';
+import GridItem from '../../components/materialComponents/Grid/GridItem';
+import GridContainer from '../../components/materialComponents/Grid/GridContainer';
+import CustomInput from '../../components/materialComponents/CustomInput/CustomInput';
+import registerImage from '../../assets/images/register.jpg';
 class Register extends Component {
   render() {
     return (
       <div>
-        <h1>
-            Register Page
-        </h1>
+        <GridItem className="register-box">
+          <div className="register-box-contents">
+            <div className="register-box-form">
+              <h1 className="welcome-back">Welcome Back</h1>
+              <GridItem className="register-input">
+                <form noValidate autoComplete="off">
+                  <CustomInput
+                    labelText="Username"
+                    id="float"
+                    formControlProps={{
+                    fullWidth: true
+                  }}
+                  />
+                  <CustomInput
+                    labelText="Password"
+                    id="float"
+                    formControlProps={{
+                    fullWidth: true
+                  }}
+                  />
+                </form>
+              </GridItem>
+              <small className="forgot-password">Forgot Password?</small>
+              <Button size="sm" className="register-button" color="primary" round>SIGN IN</Button>
+            </div>
+          </div> 
+        </GridItem>
       </div>
     );
   }
